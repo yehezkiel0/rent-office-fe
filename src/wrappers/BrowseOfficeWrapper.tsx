eb7fcaf6b7d79bb2245676414a62dc02ca022381
Yehezkiel Imannuel <91413725+yehezkiel0@users.noreply.github.com>
2025-06-23 20:55:50 +0700

    feat(landingPage): setup axios for section city and office

--- src/wrappers/BrowseOfficeWrapper.tsx
+++ src/wrappers/BrowseOfficeWrapper.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
 import OfficeCard from "../components/OfficeCard";
+import axios from "axios";
+import { Office } from "../types/type";
 export default function BrowseOfficeWrapper() {
+  const [offices, setOffices] = useState<Office[]>([]);
+
+  const [loading, setLoading] = useState(true);
+
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/offices", {
+        headers: {
+          "X-API-KEY": "awawf2244afafakkh2",
+        },
+      })
+      .then((response) => {
+        setOffices(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error loading data: {error}</p>;
+  }
   return (
     <section
       id="Fresh-Space"
@@ -11,7 +44,9 @@ export default function BrowseOfficeWrapper() {
         For Your Better Productivity.
       </h2>
       <div className="grid grid-cols-3 gap-[30px]">
-        <OfficeCard />
+        {offices.map((office) => (
+          <OfficeCard key={office.id} office={office} />
+        ))}
       </div>
     </section>
   );
